5f091207bf204a31052daada2c8c74489bdcd56c
Marc <marc489j@stud.kea.dk>
2021-08-30 16:43:30 +0200

    nameparts done

--- nameparts.js
+++ nameparts.js
@@ -5,11 +5,9 @@ const fullname = "Marc Ladefoged Jensen";
 const firstSpace = fullname.indexOf(" ");
 const lastSpace = fullname.lastIndexOf(" ");
 
-let firstname = fullname.substring(0, firstSpace).trim();
-let middlename = fullname.substring(firstSpace, lastSpace).trim();
-let lastname = fullname.substring(lastSpace).trim();
-
-
+let firstname = fullname.substring(0, firstSpace);
+let middlename = fullname.substring(firstSpace+1, lastSpace);
+let lastname = fullname.substring(lastSpace+1);
 
 console.log(`His first name is ${firstname}`);
 console.log(`His middlename is ${middlename}`);
---
+++ realcapitalization.js
@@ -0,0 +1,5 @@
+const name ="Marc";
+
+console.log()
+
+name.substring(0,1).toUpperCase()+name.substring(1).toLowerCase()
