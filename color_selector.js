b9a7d3a92d6d169c83498b7ddae745827252e764
Marc <marc489j@stud.kea.dk>
2021-09-15 21:47:20 +0200

    started on color selector

---
+++ color_selector.js
@@ -0,0 +1,109 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed");
+
+  document.querySelector("#color").addEventListener("input", displayCursorColor);
+  /* document.querySelector("#color").addEventListener("change", displayCursorColor); */
+
+  //gets hex code from colorpicker
+  function getCursorColor() {
+    let colorInput = document.querySelector("#color");
+    colorInput = colorInput.value;
+    console.log(colorInput);
+    return colorInput;
+  }
+
+  //display color from the color picker
+  function displayCursorColor() {
+    let color = getCursorColor();
+    let displayColor = document.querySelector(".selected_color");
+    displayColor.style.backgroundColor = color;
+    console.log(color);
+    displayHEX(color);
+    displayRGB(color);
+    displayHSL(color);
+  }
+
+  // display hex code from color picker
+  function displayHEX(color) {
+    console.log("this is some color " + color);
+   let hexDiv = document.querySelector(".color_hex");
+   hexDiv.innerHTML = `HEX: ${color}`;
+
+  }
+
+  function hexToRGB(hex) {
+    let r, g, b;
+    if (hex.length == 4) {
+      r = `0x${hex[1]}${hex[1]}`;
+      g = `0x${hex[2]}${hex[2]}`;
+      b = `0x${hex[3]}${hex[3]}`;
+    } else if (hex.length == 7) {
+      r = `0x${hex[1]}${hex[2]}`;
+      g = `0x${hex[3]}${hex[4]}`;
+      b = `0x${hex[5]}${hex[6]}`;
+    }
+  
+    r = parseInt(r);
+    g = parseInt(g);
+    b = parseInt(b);
+  
+    let rgb = { r: r, g: g, b: b };
+    /* console.log(rgb); */
+    return rgb;
+  }
+
+  function displayRGB(color) {
+    let rgbColor = hexToRGB(color);
+    console.log("rgb color from hex ");
+    rgbColor = `${rgbColor.r}, ${rgbColor.g}, ${rgbColor.b}`;
+    console.log(rgbColor);
+    let rgbDiv = document.querySelector(".color_rgb p");
+    rgbDiv.innerHTML = `RGB: ${rgbColor}`;
+  }
+
+
+  
+  function displayHSL(color) {
+
+    r /= 255;
+    g /= 255;
+    b /= 255;
+  
+    let h, s, l;
+  
+    const min = Math.min(r,g,b);
+    const max = Math.max(r,g,b);
+   
+    if( max === min ) {
+      h = 0;
+    } else
+    if (max === r) {
+      h = 60 * (0 + (g - b) / (max - min) );
+    } else
+    if (max === g) {
+      h = 60 * (2 + (b - r) / (max - min) );
+    } else
+    if (max === b) {
+      h = 60 * (4 + (r - g) / (max - min) );
+    }
+   
+    if (h < 0) {h = h + 360; }
+   
+    l = (min + max) / 2;
+   
+    if (max === 0 || min === 1 ) {
+      s = 0;
+    } else {
+      s = (max - l) / ( Math.min(l,1-l));
+    }
+    // multiply s and l by 100 to get the value in percent, rather than [0,1]
+    s *= 100;
+    l *= 100;
+  
+    console.log("hsl(%f,%f%,%f%)", h, s, l); // just for testing
+
+  }
+  
+});
