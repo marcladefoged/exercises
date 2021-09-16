885251ebcbcb6c994f9953ca9a4c6681503c5cc0
Marc <marc489j@stud.kea.dk>
2021-09-16 18:38:10 +0200

    color selector done

--- color_selector.js
+++ color_selector.js
@@ -25,16 +25,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     displayHSL(color);
   }
 
-  // display hex code from color picker
-  function displayHEX(color) {
-    console.log("this is some color " + color);
-   let hexDiv = document.querySelector(".color_hex");
-   hexDiv.innerHTML = `HEX: ${color}`;
-
-  }
-
   function hexToRGB(hex) {
-    let r, g, b;
     if (hex.length == 4) {
       r = `0x${hex[1]}${hex[1]}`;
       g = `0x${hex[2]}${hex[2]}`;
@@ -44,11 +35,11 @@ window.addEventListener("DOMContentLoaded", (event) => {
       g = `0x${hex[3]}${hex[4]}`;
       b = `0x${hex[5]}${hex[6]}`;
     }
-  
+
     r = parseInt(r);
     g = parseInt(g);
     b = parseInt(b);
-  
+
     let rgb = { r: r, g: g, b: b };
     /* console.log(rgb); */
     return rgb;
@@ -63,47 +54,65 @@ window.addEventListener("DOMContentLoaded", (event) => {
     rgbDiv.innerHTML = `RGB: ${rgbColor}`;
   }
 
-
-  
-  function displayHSL(color) {
+  let r, g, b;
+  function rgbToHsl(color) {
+    /* let rgbColor = hexToRGB(color); */
 
     r /= 255;
     g /= 255;
     b /= 255;
-  
+
     let h, s, l;
-  
-    const min = Math.min(r,g,b);
-    const max = Math.max(r,g,b);
-   
-    if( max === min ) {
+
+    const min = Math.min(r, g, b);
+    const max = Math.max(r, g, b);
+
+    if (max === min) {
       h = 0;
-    } else
-    if (max === r) {
-      h = 60 * (0 + (g - b) / (max - min) );
-    } else
-    if (max === g) {
-      h = 60 * (2 + (b - r) / (max - min) );
-    } else
-    if (max === b) {
-      h = 60 * (4 + (r - g) / (max - min) );
+    } else if (max === r) {
+      h = 60 * (0 + (g - b) / (max - min));
+    } else if (max === g) {
+      h = 60 * (2 + (b - r) / (max - min));
+    } else if (max === b) {
+      h = 60 * (4 + (r - g) / (max - min));
     }
-   
-    if (h < 0) {h = h + 360; }
-   
+
+    if (h < 0) {
+      h = h + 360;
+    }
+
     l = (min + max) / 2;
-   
-    if (max === 0 || min === 1 ) {
+
+    if (max === 0 || min === 1) {
       s = 0;
     } else {
-      s = (max - l) / ( Math.min(l,1-l));
+      s = (max - l) / Math.min(l, 1 - l);
     }
     // multiply s and l by 100 to get the value in percent, rather than [0,1]
     s *= 100;
     l *= 100;
-  
+
     console.log("hsl(%f,%f%,%f%)", h, s, l); // just for testing
 
+    /*  return `${Math.round(h * 10) / 10}, ${Math.round(s * 10) / 10}%, ${Math.round(l * 10) / 10}%`; */
+    /*   console.log("hsl(%f,%f%,%f%)", h, s, l); // just for testing */
+    return { h, s, l };
   }
-  
+
+  function displayHSL(color) {
+    let hslColor = rgbToHsl(color);
+    let h = Math.round(hslColor.h * 10) / 10;
+    let s = Math.round(hslColor.s * 10) / 10;
+    let l = Math.round(hslColor.l * 10) / 10;
+    hslColor = `HSL: ${h}, ${s}%, ${l}%`;
+    let hslDiv = document.querySelector(".color_hsl p");
+    hslDiv.innerHTML = hslColor;
+    console.log(hslColor);
+  }
+
+  function displayHEX(color) {
+    console.log("this is some color " + color);
+    let hexDiv = document.querySelector(".color_hex p");
+    hexDiv.innerHTML = `HEX: ${color}`;
+  }
 });
