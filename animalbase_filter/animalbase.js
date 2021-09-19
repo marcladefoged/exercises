e9bd33fb5be82f215d06508a2206f936db641ef6
Marc <marc489j@stud.kea.dk>
2021-09-20 00:05:59 +0200

    sort toggle direction

--- animalbase_filter/animalbase.js
+++ animalbase_filter/animalbase.js
@@ -85,37 +85,55 @@ function isDog (animal) {
 
 function selectSort(event) {
     const sortBy = event.target.dataset.sort;
-    console.log(`User selected ${sortBy}`);
-    sortList(sortBy);
+    const sortDir = event.target.dataset.sortDirection;
+
+    // toggle the direction!
+    if (sortDir === "asc") {
+        event.target.dataset.sortDirection = "desc";
+    } else {
+        event.target.dataset.sortDirection = "asc";
+    }
+
+    console.log(`User selected ${sortBy} - ${sortDir}`);
+    sortList(sortBy, sortDir);
 }
 
-function sortList(sortBy) {
+function sortList(sortBy, sortDir) {
     let sortedList = allAnimals;
+    let direction = 1;
+    if (sortDir === "desc") {
+        direction = -1;
+    } else {
+        direction = 1;
+    }
 
-    if (sortBy === "name") {
+    // if (sortBy === "name") {
+        sortedList = sortedList.sort(sortByProperty);
+/*     } else if( sortBy === "type") {
         sortedList = sortedList.sort(sortByName);
-    } else if( sortBy === "type") {
-        sortedList = sortedList.sort(sortByType);
+    } */
+
+
+    function sortByProperty(animalA, animalB) {
+        // console.log(`sortBy is ${sortBy}`);
+        if (animalA[sortBy] < animalB[sortBy]) {
+            return -1 * direction;
+        } else {
+            return 1 * direction;
+        }
     }
     displayList(sortedList);
 }
 
-function sortByName(animalA, animalB) {
-    if (animalA.name < animalB.name) {
-        return -1;
-    } else {
-        return 1;
-    }
-}
-
-function sortByType(animalA, animalB) {
+    // Hardcoded sorting by type
+/* function sortByType(animalA, animalB) {
     if (animalA.type < animalB.type) {
         return -1;
     } else {
         return 1;
     }
 }
-
+ */
 
 function displayList(animals) {
     // clear the list
